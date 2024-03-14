c790533d2034091c16e246d6bf9ba0c090fb8b21
Jeeva Kandasamy <jkandasa@gmail.com>
2024-03-14 11:21:13 +0530

    fix lint issue
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- cmd/plugin/openshift/catalog_source/install.go
+++ cmd/plugin/openshift/catalog_source/install.go
@@ -32,7 +32,7 @@ func init() {
 var installCsCmd = &cobra.Command{
 	Use:   "catalog-source",
 	Short: "installs catalog source",
-	Args:  cobra.ExactValidArgs(1),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, catalogSourceNames []string) {
 		catalogSourceName := catalogSourceNames[0]
 		// get kubernetes client
